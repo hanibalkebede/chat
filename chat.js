f78ff8d01fae5638a236c95a6fd06488c24b5010
hanibalkebede <anibal135789@gmail.com>
2026-03-30 13:33:56 -0400

    Add initial HTML structure and create playingscreen.js file for game logic

---
+++ chat.js
@@ -0,0 +1,64 @@
+// Define the various screens that the game has
+const INTRO_SCREEN = 0
+const PLAYING_SCREEN = 1
+const GAMEOVER_SCREEN = 2
+let currentScreen = INTRO_SCREEN
+let pewpewSound, hitSound, explosionSound
+let backgroundImage
+
+// This function is run before the setup function to load things that could take a while to load
+// like sounds and images (from files)
+function preload() {
+	// Do all the image and sound file loading here
+	// pewpewSound = loadSound('pew-pew.mp3')
+	// hitSound = loadSound('hit.mp3')
+	// explosionSound = loadSound('explosion.mp3')
+	backgroundImage = loadImage('background.webp')
+} // End of preload function
+
+// This function is run once when the program starts
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	background('background.jpg')
+	frameRate(20)
+	
+	// // Setup the player
+	// playerPosition = createVector(windowWidth/2, windowHeight - 2*playerRadius)
+	
+	// // Setup enemies
+	// for (let i = 0; i < numEnemies; i++) {
+	// 	let x = (i + 1) * 100
+	// 	let y = random(100, 500)
+	// 	enemyPositions.push(createVector(x, y))
+	// 	let enemySpeed = random(5, 10)
+	// 	enemySpeeds.push(enemySpeed)
+	// 	enemyHitCounts.push(0)
+	// }
+} // End of setup function
+
+// This function is run repeatedly, based on the set frame rate
+function draw() {
+	// This if-else block does exactly the same as the switch block below
+	// if (currentScreen == INTRO_SCREEN) {
+	// 		drawIntroScreen()		
+	// }
+	// else if(currentScreen == PLAYING_SCREEN) {
+	// 		drawPlayingScreen()
+	// }
+	// else if(currentScreen == GAMEOVER_SCREEN) {
+	// 		drawGameOverScreen()
+	// }
+	
+	// Draw the current screen
+	switch(currentScreen) {
+		case INTRO_SCREEN:
+			drawIntroScreen()
+			break;
+		case PLAYING_SCREEN:
+			drawPlayingScreen()
+			break;
+		case GAMEOVER_SCREEN:
+			drawGameOverScreen()
+			break;
+	}
+} // End of draw function
